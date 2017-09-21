8a48aaf728fa2042e0dae90423607f1372d96923
Hiromasa Kakehashi (hkakehas) <hkakehas@cisco.com>
2017-09-22 04:49:15 +0900

    complete pj4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Initialize stat which takes either 0 or -1
+@stat
+M=0
+
+// Check keyboard input
+(CHECKINPUT)
+@KBD
+D=M
+
+@FILL // Detect input. Jumpt to FILL
+D;JNE
+
+@CLEAR // No input. Jump to CLEAR
+D;JEQ
+
+(FILL)
+@SCREEN
+D=M
+
+@CHECKINPUT // Back to CHECKINPUT if it is black
+D;JNE
+
+@stat
+M=-1
+@FLIPBITS
+0;JMP
+
+(CLEAR)
+@SCREEN
+D=M
+
+@CHECKINPUT // Back to CHECKINPUT if it is clear
+D;JEQ
+
+@stat
+M=0
+@FLIPBITS
+0;JMP
+
+(FLIPBITS)
+// Initialize vars.
+@SCREEN
+D=A
+@addr
+M=D
+
+@i
+M=0
+
+@8192
+D=A
+@j
+M=D
+
+(LOOP)
+@j
+D=M
+@i
+D=D-M
+@CHECKINPUT // if i >= 8192 goto CHECKINPUT
+D;JLE
+
+@stat // Change the register value with the value of stat
+D=M
+@addr
+A=M
+M=D
+
+@i
+M=M+1
+@addr
+M=M+1
+
+@LOOP
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,36 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//  Pseudocode
+//  for i := 0; i < R1; i++ {
+//    R2 = R2 + R0
+//  }
+
+@i
+M=0
+@R2
+M=0 // Initialize i and R2
+
+(LOOP)
+
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JGE // if (i - R1) >= 0 goto END
+
+@R0
+D=M
+@R2
+M=M+D // R2 = R2 + R0
+
+@i
+M=M+1 // i++
+
+@LOOP
+0;JMP // goto LOOP
+
+(END)
+@END
+0;JMP // goto LOOP
